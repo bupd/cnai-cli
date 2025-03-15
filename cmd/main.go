727f98d0a136ae0b776e3f7188bc3118159eb7a1
bupd <bupdprasanth@gmail.com>
2025-03-15 08:58:53 +0530

    update main package
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bupd/cnai-cli/pkg/builder"
+	"github.com/bupd/cnai-cli/pkg/cloner"
+	"github.com/bupd/cnai-cli/pkg/modelfile"
+	"github.com/bupd/cnai-cli/pkg/pusher"
+	"github.com/bupd/cnai-cli/pkg/storage"
+	"github.com/bupd/cnai-cli/pkg/utils"
+)
 
 func main() {
 	fmt.Println("I am back")
+
+	origin := os.Args[1]
+	destination := utils.RemoveHttps(os.Args[2])
+
+	storage.GetCurrDir()
+	// download stage
+	modelDir := cloner.DownloadModel(origin)
+  // GenerateModelFile
+  modelfile.GotoModelDir(modelDir)
+	modelfile.GenerateModelFile()
+	// build stage
+	builder.BuildModelPackage(destination)
+	// push stage
+	pusher.PushModel(destination)
 }
