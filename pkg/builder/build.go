d32383d93190ab88def233a400508042ddd1f91b
bupd <bupdprasanth@gmail.com>
2025-03-16 07:31:20 +0530

    update build
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pkg/builder/build.go
+++ pkg/builder/build.go
@@ -1,12 +1,14 @@
 package builder
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/sirupsen/logrus"
 )
 
 func BuildModelPackage(destination string) {
+  fmt.Println("building model")
 	cmd := exec.Command("modctl", "build", ".", "-t", destination)
 	err := cmd.Run()
 	if err != nil {
