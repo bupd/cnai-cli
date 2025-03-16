8eb0f7eac7b3fe6ff4eb1edaf9d02e139253eca6
bupd <bupdprasanth@gmail.com>
2025-03-16 07:35:54 +0530

    update main with help command
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -13,16 +13,22 @@ import (
 )
 
 func main() {
-	fmt.Println("I am back")
 
 	origin := os.Args[1]
 	destination := utils.RemoveHttps(os.Args[2])
 
+	if len(origin) < 5 || len(destination) < 1 {
+		fmt.Println("Please provide the correct args for the command")
+		fmt.Println("Example")
+		fmt.Println("cnai-cli 'hugging-face_url' 'destination_oci_reg_url'")
+    return
+	}
+
 	storage.GetCurrDir()
 	// download stage
 	modelDir := cloner.DownloadModel(origin)
-  // GenerateModelFile
-  modelfile.GotoModelDir(modelDir)
+	// GenerateModelFile
+	modelfile.GotoModelDir(modelDir)
 	modelfile.GenerateModelFile()
 	// build stage
 	builder.BuildModelPackage(destination)
